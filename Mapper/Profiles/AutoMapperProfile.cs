e8f9c4fd8a05be601e696d46aad4cb02063aa7a9
henry23 <hobregon23@users.noreply.github.com>
2022-09-21 11:06:07 -0400

    Update AutoMapperProfile.cs

--- Mapper/Profiles/AutoMapperProfile.cs
+++ Mapper/Profiles/AutoMapperProfile.cs
@@ -8,6 +8,7 @@ namespace OnlineStore.Mapper.Profiles
         public AutoMapperProfile()
         {
             CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
         }
     }
 }
