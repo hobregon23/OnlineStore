515dba7152981ddda495983eacb434622a9d5aac
henry23 <hobregon23@users.noreply.github.com>
2022-10-07 21:02:06 -0400

    cuenta bancaria en factura

--- Models/Dto/Check_Out.cs
+++ Models/Dto/Check_Out.cs
@@ -10,11 +10,6 @@ namespace OnlineStore.Models
         public string Full_name { get; set; }
         public Address Address { get; set; } = new Address() { Province = new Address_Province() };
         public decimal Shipping_price { get; set; }
-        //public string Address_line { get; set; }
-        //public string State { get; set; }
-        //public string City { get; set; }
-        //public string Postal_code { get; set; }
-        //public int Province_id { get; set; }
 
     }
 }
